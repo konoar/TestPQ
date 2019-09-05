7a05666eb09f28c506f8e35108a5e7ce3cf0feaa
konoar <arata@kono.site>
2019-09-05 20:10:08 +0900

    Init

---
+++ CreateSchema.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+#########################################################
+#
+# CreateSchema.sh
+#   Copyright 2019.09.05 Konoar
+#
+#########################################################
+
+# Drop
+psql -U postgres <<END
+DROP DATABASE IF EXISTS ${USER};
+DROP ROLE     IF EXISTS ${USER};
+END
+
+# Create
+psql -U postgres <<END
+CREATE ROLE     ${USER} WITH LOGIN;
+CREATE DATABASE ${USER} WITH OWNER ${USER};
+END
+
+psql -U ${USER}  <<END
+
+CREATE SCHEMA TestPQ;
+
+CREATE SEQUENCE TestPQ.S_TEST START WITH 1 INCREMENT BY 1;
+
+CREATE FUNCTION TestPQ.F_GET_DEFAULT_TID() RETURNS cHAR(8) AS
+\$\$
+
+    SELECT 'TID' || TO_CHAR(NEXTVAL('TestPQ.S_TEST'), 'FM00000');
+
+\$\$ LANGUAGE SQL;
+
+CREATE TABLE TestPQ.R_TEST
+(
+    tid     CHAR(8) PRIMARY KEY DEFAULT TestPQ.F_GET_DEFAULT_TID(),
+    value   VARCHAR(16) NOT NULL,
+    ctime   TIMESTAMP DEFAULT LOCALTIMESTAMP
+)
+WITH
+(
+    FILLFACTOR = 90
+);
+
+CREATE INDEX ON TESTPQ.R_TEST
+(
+    value
+);
+
+CREATE FUNCTION TestPQ.F_SELECT_TEST(target TIMESTAMP)
+    RETURNS TABLE(rid BIGINT, tid CHAR(8), value VARCHAR(16)) AS
+\$\$
+
+    SELECT
+        ROW_NUMBER() OVER W AS rid,
+        r1.tid              AS tid,
+        r1.value            AS value
+    FROM
+        TestPQ.R_TEST r1
+    WHERE
+       r1.ctime <= target 
+    WINDOW
+        W AS(ORDER BY r1.value ASC)
+
+\$\$ LANGUAGE SQL;
+
+INSERT INTO TestPQ.R_TEST
+(
+    value,
+    ctime
+)
+VALUES
+    ('bbb', CAST(CURRENT_DATE - 2 AS TIMESTAMP)),
+    ('ccc', CAST(CURRENT_DATE - 1 AS TIMESTAMP)),
+    ('aaa', CAST(CURRENT_DATE - 0 AS TIMESTAMP)),
+    ('ddd', CAST(CURRENT_DATE + 1 AS TIMESTAMP));
+
+END
+
---
+++ main.c
@@ -0,0 +1,265 @@
+/****************************************************************
+
+   main.c
+     copyright 2019.10.05 konoar
+
+ ****************************************************************/
+#include "libpq-fe.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define KS_OK 0
+#define KS_NG 1
+
+int ksInsert(PGconn *conn)
+{
+
+    PGresult *res = NULL;
+    int retval = KS_OK;
+
+    do {
+
+        // BEGIN TRANSACTION
+
+        res = PQexec(conn,
+            "BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED READ WRITE");
+
+        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
+            retval = KS_NG;
+            break;
+        }
+
+        PQclear(res);
+        res = NULL;
+
+        // INSERT
+ 
+        res = PQexec(conn,
+                "INSERT INTO TestPQ.R_TEST(value)"
+                "values ('eee'), ('fff')"
+              );
+
+        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
+            retval = KS_NG;
+            break;
+        }
+
+        PQclear(res);
+        res = NULL;
+
+        // COMMIT
+
+        res = PQexec(conn, "COMMIT");
+
+        if (PGRES_COMMAND_OK != PQresultStatus(res)) {
+            retval = KS_NG;
+            break;
+        }
+
+    } while(0);
+
+    if (res) {
+        PQclear(res);
+        res = NULL;
+    }
+
+    return retval;
+
+}
+
+ int ksSelectA(PGconn *conn)
+ {
+
+    PGresult *res = NULL;
+    int retval = KS_OK, ret;
+
+    putc('\n', stdout);
+
+    do {
+
+        // BEGIN TRANSACTION
+
+        res = PQexec(conn,
+            "BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED READ ONLY");
+
+        if (PGRES_COMMAND_OK != (ret = PQresultStatus(res))) {
+            retval = KS_NG;
+            break;
+        }
+
+        PQclear(res);
+        res = NULL;
+
+        // DECLARE CURSOR
+
+        res = PQexec(conn,
+            "DECLARE mycursor CURSOR FOR "
+              "SELECT "
+                "rid,"
+                "tid,"
+                "value "
+              "FROM "
+                "TestPQ.F_SELECT_TEST(LOCALTIMESTAMP)"
+            );
+
+        if (PGRES_COMMAND_OK != (ret = PQresultStatus(res))) {
+            retval = KS_NG;
+            break;
+        }
+
+        PQclear(res);
+        res = NULL;
+
+        // FETCH
+
+        res = PQexec(conn, "FETCH ALL IN mycursor");
+
+        if (PGRES_TUPLES_OK != PQresultStatus(res)) {
+            retval = KS_NG;
+            break;
+        }
+
+        for (int idx = 0; idx < PQntuples(res); idx++) {
+            printf("%-10s%-16s%-16s\n",
+                PQgetvalue(res, idx, 0),
+                PQgetvalue(res, idx, 1),
+                PQgetvalue(res, idx, 2));
+        }
+
+        PQclear(res);
+        res = NULL;
+
+        // END TRANSACTION
+
+        res = PQexec(conn, "END TRANSACTION");
+
+        if (PGRES_COMMAND_OK != (ret = PQresultStatus(res))) {
+            retval = KS_NG;
+            break;
+        }
+
+    } while(0);
+
+    if (res) {
+        PQclear(res);
+        res = NULL;
+    }
+
+    putc('\n', stdout);
+
+    return retval;
+
+ }
+
+ int ksSelectB(PGconn *conn)
+ {
+
+    PGresult *res = NULL;
+    int retval = KS_OK, ret;
+
+    putc('\n', stdout);
+
+    do {
+
+        // BEGIN TRANSACTION
+
+        res = PQexec(conn,
+            "BEGIN TRANSACTION ISOLATION LEVEL READ COMMITTED READ ONLY");
+
+        if (PGRES_COMMAND_OK != (ret = PQresultStatus(res))) {
+            retval = KS_NG;
+            break;
+        }
+
+        PQclear(res);
+        res = NULL;
+
+        // SELECT
+
+        if (0 == PQsendQuery(conn,
+            "SELECT "
+              "rid,"
+              "tid,"
+              "value "
+            "FROM "
+              "TestPQ.F_SELECT_TEST(LOCALTIMESTAMP)"
+            )) {
+
+            retval = KS_NG;
+            break;
+        }
+
+        PQsetSingleRowMode(conn);
+
+        while (res = PQgetResult(conn)) {
+
+            if (PGRES_SINGLE_TUPLE == PQresultStatus(res)) {
+                printf("%-10s%-16s%-16s\n",
+                    PQgetvalue(res, 0, 0),
+                    PQgetvalue(res, 0, 1),
+                    PQgetvalue(res, 0, 2));
+            }
+
+            PQclear(res);
+            res = NULL;
+
+        }
+
+        // END TRANSACTION
+
+        res = PQexec(conn, "END TRANSACTION");
+
+        if (PGRES_COMMAND_OK != (ret = PQresultStatus(res))) {
+            retval = KS_NG;
+            break;
+        }
+
+    } while(0);
+
+    if (res) {
+        PQclear(res);
+        res = NULL;
+    }
+
+    putc('\n', stdout);
+
+    return KS_OK;
+
+ }
+
+int main(int argc, const  char* argv[])
+{
+
+    int ret = KS_OK;
+
+    char buff[256];
+    PGconn *conn;
+
+    sprintf(
+        buff, "postgresql://%s@localhost:5432/%s",
+        getenv("USER"), getenv("USER"));
+
+    conn = PQconnectdb(buff);
+
+    if (CONNECTION_OK != PQstatus(conn)) {
+        return KS_NG;
+    }
+
+    do {
+
+        if (KS_OK != (ret = ksInsert(conn))) {
+            break;
+        }
+
+        if (KS_OK != (ret = ksSelectB(conn))) {
+            break;
+        }
+
+    } while(0);
+
+    PQfinish(conn);
+
+    return ret;
+
+}
+
